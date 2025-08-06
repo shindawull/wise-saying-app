ac7afecd101e429dbca22fa07d670fc89d8cc7bd
shindawull <juoiy05@naver.com>
2025-08-07 07:14:29 +0900

    work

--- src/main/java/com/dawull/Main.java
+++ src/main/java/com/dawull/Main.java
@@ -24,6 +24,11 @@ class App {
 
             if (cmd.equals("종료")) {
                 break;
+            } else if (cmd.equals("등록")) {
+                System.out.print("명언 : ");
+                String content = scanner.nextLine();
+                System.out.print("작가 : ");
+                String author = scanner.nextLine();
             }
         }
         scanner.close();
