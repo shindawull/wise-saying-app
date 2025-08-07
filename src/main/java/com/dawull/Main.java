ff0925b86b194ffad2cd876fef428ce5ab8a318d
shindawull <juoiy05@naver.com>
2025-08-07 20:48:08 +0900

    try catch null point exception

--- src/main/java/com/dawull/Main.java
+++ src/main/java/com/dawull/Main.java
@@ -44,7 +44,12 @@ class App {
             } else if (cmd.equals("목록")) {
                 System.out.println(" 번호 / 작가 / 명언 ");
                 System.out.println("----------------------");
-                System.out.println("%d / %s / %s".formatted(lastWiseSaying.id, lastWiseSaying.content, lastWiseSaying.author));
+                try {
+                    System.out.println("%d / %s / %s".formatted(lastWiseSaying.id, lastWiseSaying.content, lastWiseSaying.author));
+                } catch (NullPointerException e) {
+                    System.out.println("등록된 명언이 없습니다.");
+                }
+
 
             }
         }
