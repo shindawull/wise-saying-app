d424477b1f36edf27db679580d45a69abb39393d
shindawull <juoiy05@naver.com>
2025-08-13 04:15:27 +0900

    work makeSampleData함수도입으로 의도 명확히 전달

--- src/main/java/com/dawull/App.java
+++ src/main/java/com/dawull/App.java
@@ -21,8 +21,8 @@ public class App {
     public void run() {
         System.out.println("== 명어 앱 ==");
 
-        addWiseSaying("나의 죽음을 적들에게 알리지 마라.", "이순신장군");
-        addWiseSaying("삶이 있는 한 희망은 있다.", "키케로");
+        makeSampleData();
+
         while (true) {
             System.out.print("명령) ");
             String cmd = scanner.nextLine();
@@ -42,14 +42,17 @@ public class App {
         scanner.close();
     }
 
+    private void makeSampleData() {
+        addWiseSaying("나의 죽음을 적들에게 알리지 마라.", "이순신장군");
+        addWiseSaying("삶이 있는 한 희망은 있다.", "키케로");
+    }
+
     private WiseSaying addWiseSaying(String content, String author) {
         int id = ++lastId;
 
         WiseSaying wiseSaying = new WiseSaying(id, content, author);
 
         wiseSayings.add(wiseSaying);
-
-        System.out.println("wiseSayings = " + wiseSayings);
 
         return wiseSaying;
     }
