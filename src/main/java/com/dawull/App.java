3e5d21bdc0a4b2bc7dd314d2790c9c018b5ae124
shindawull <juoiy05@naver.com>
2025-08-13 08:17:57 +0900

    work

--- src/main/java/com/dawull/App.java
+++ src/main/java/com/dawull/App.java
@@ -40,42 +40,11 @@ public class App {
             } else if (cmd.startsWith("삭제?id=")) {
                 wiseSayingController.actionDelete(cmd, wiseSayings);
             } else if (cmd.startsWith("수정?id=")) {
-                String id = cmd.substring(6);
-                actionModify(Integer.parseInt(id));
+                wiseSayingController.actionModify(cmd, wiseSayings);
             }
         }
         scanner.close();
     }
-
-    private void actionModify(int id) {
-        WiseSaying foundWaseSaying = null;
-
-        for (WiseSaying wiseSaying : wiseSayings) {
-            if (wiseSaying.getId() == id) {
-                foundWaseSaying = wiseSaying;
-                break;
-            }
-        }
-
-        if (foundWaseSaying == null) {
-            System.out.println("%d번 명언은 존재하지 않습니다.".formatted(id));
-            return;
-        }
-        System.out.println("명언(기존) : %s".formatted(foundWaseSaying.getContent()));
-        System.out.print("새로운 명언 : ");
-        String content = scanner.nextLine();
-
-        System.out.println("작사(기존) : %s".formatted(foundWaseSaying.getAuthor()));
-        System.out.print("새로운 작사 : ");
-        String author = scanner.nextLine();
-
-        foundWaseSaying.setContent(content);
-        foundWaseSaying.setAuthor(author);
-        System.out.println("%d번 명언이 수정되었습니다.".formatted(id));
-    }
-
-
-
 
     private void makeSampleData() {
         addWiseSaying("나의 죽음을 적들에게 알리지 마라.", "이순신장군");
--- src/main/java/com/dawull/domain/wiseSaying/controller/WiseSayingController.java
+++ src/main/java/com/dawull/domain/wiseSaying/controller/WiseSayingController.java
@@ -3,17 +3,24 @@ package com.dawull.domain.wiseSaying.controller;
 import com.dawull.domain.wiseSaying.entity.WiseSaying;
 
 import java.util.List;
+import java.util.Scanner;
 
 
 public class WiseSayingController {
-        public void actionList(List<WiseSaying> wiseSayings) {
-            System.out.println(" 번호 / 작가 / 명언 ");
-            System.out.println("------------------------");
+    Scanner scanner;
 
-            for (WiseSaying wiseSaying : wiseSayings.reversed()) {
-                System.out.println("%d / %s / %s".formatted(wiseSaying.getId(), wiseSaying.getContent(), wiseSaying.getAuthor()));
-            }
+    public WiseSayingController(){
+        scanner = new Scanner(System.in);
+    }
+
+    public void actionList(List<WiseSaying> wiseSayings) {
+        System.out.println(" 번호 / 작가 / 명언 ");
+        System.out.println("------------------------");
+
+        for (WiseSaying wiseSaying : wiseSayings.reversed()) {
+            System.out.println("%d / %s / %s".formatted(wiseSaying.getId(), wiseSaying.getContent(), wiseSaying.getAuthor()));
         }
+    }
 
     public void actionDelete(String cmd, List<WiseSaying> wiseSayings) {
         String idStr = cmd.substring(6);
@@ -22,5 +29,34 @@ public class WiseSayingController {
 
         if (removed) System.out.println("%d번 명언을 삭제했습니다.".formatted(id));
         else System.out.println("%d번 명언은 존재하지않습니다.".formatted(id));
+    }
+
+    public void actionModify(String cmd, List<WiseSaying> wiseSayings) {
+        String idStr = cmd.substring(6);
+        int id = Integer.parseInt(idStr);
+        WiseSaying foundWaseSaying = null;
+
+        for (WiseSaying wiseSaying : wiseSayings) {
+            if (wiseSaying.getId() == id) {
+                foundWaseSaying = wiseSaying;
+                break;
+            }
+        }
+
+        if (foundWaseSaying == null) {
+            System.out.println("%d번 명언은 존재하지 않습니다.".formatted(id));
+            return;
+        }
+        System.out.println("명언(기존) : %s".formatted(foundWaseSaying.getContent()));
+        System.out.print("새로운 명언 : ");
+        String content = scanner.nextLine();
+
+        System.out.println("작사(기존) : %s".formatted(foundWaseSaying.getAuthor()));
+        System.out.print("새로운 작사 : ");
+        String author = scanner.nextLine();
+
+        foundWaseSaying.setContent(content);
+        foundWaseSaying.setAuthor(author);
+        System.out.println("%d번 명언이 수정되었습니다.".formatted(id));
     }
 }
