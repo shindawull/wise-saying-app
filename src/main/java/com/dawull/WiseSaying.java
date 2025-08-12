f646176ddc05556e17bdf11b925c8b57108e89f4
shindawull <juoiy05@naver.com>
2025-08-13 02:16:22 +0900

    work Refactor

---
+++ src/main/java/com/dawull/WiseSaying.java
@@ -0,0 +1,18 @@
+package com.dawull;
+
+class WiseSaying {
+    public int id;
+    public String content;
+    public String author;
+
+    public WiseSaying(int id, String content, String author) {
+        this.id = id;
+        this.content = content;
+        this.author = author;
+    }
+
+    @Override
+    public String toString() {
+        return "WiseSaying (id=%d, content=\"%s\", author=\"%s\")".formatted(id, content, author);
+    }
+}
