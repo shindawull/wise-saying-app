8d3d72d4b5042e8a275bb972c8e7310571f7d12a
shindawull <juoiy05@naver.com>
2025-08-13 19:15:43 +0900

    work

--- src/main/java/com/dawull/App.java
+++ src/main/java/com/dawull/App.java
@@ -1,30 +1,24 @@
 package com.dawull;
 
 import com.dawull.domain.wiseSaying.controller.WiseSayingController;
-import com.dawull.domain.wiseSaying.entity.WiseSaying;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Scanner;
 
 public class App {
     // 변수 선언
     private final Scanner scanner;
-
-    private final List<WiseSaying> wiseSayings;
     private WiseSayingController wiseSayingController;
 
     // 변수 초기화
     public App() {
         scanner = new Scanner(System.in);
-        wiseSayings = new ArrayList<>();
         wiseSayingController = new WiseSayingController();
     }
 
     public void run() {
         System.out.println("== 명어 앱 ==");
 
-        wiseSayingController.makeSampleData(wiseSayings);
+        wiseSayingController.makeSampleData();
 
         while (true) {
             System.out.print("명령) ");
@@ -33,13 +27,13 @@ public class App {
             if (cmd.equals("종료")) {
                 break;
             } else if (cmd.equals("등록")) {
-                wiseSayingController.actionAdd(scanner, wiseSayings);
+                wiseSayingController.actionAdd(scanner);
             } else if (cmd.equals("목록")) {
-                wiseSayingController.actionList(wiseSayings);
+                wiseSayingController.actionList();
             } else if (cmd.startsWith("삭제?id=")) {
-                wiseSayingController.actionDelete(cmd, wiseSayings);
+                wiseSayingController.actionDelete(cmd);
             } else if (cmd.startsWith("수정?id=")) {
-                wiseSayingController.actionModify(scanner, cmd, wiseSayings);
+                wiseSayingController.actionModify(scanner, cmd);
             }
         }
         scanner.close();
--- src/main/java/com/dawull/domain/wiseSaying/controller/WiseSayingController.java
+++ src/main/java/com/dawull/domain/wiseSaying/controller/WiseSayingController.java
@@ -2,18 +2,21 @@ package com.dawull.domain.wiseSaying.controller;
 
 import com.dawull.domain.wiseSaying.entity.WiseSaying;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
 
 public class WiseSayingController {
     private int lastId;
+    private final List<WiseSaying> wiseSayings;
 
     public WiseSayingController(){
-
+        lastId = 0;
+        wiseSayings = new ArrayList<>();
     }
 
-    public void makeSampleData(List<WiseSaying> wiseSayings) {
+    public void makeSampleData() {
         addWiseSaying(wiseSayings,"나의 죽음을 적들에게 알리지 마라.", "이순신장군");
         addWiseSaying(wiseSayings,"삶이 있는 한 희망은 있다.", "키케로");
     }
@@ -28,7 +31,7 @@ public class WiseSayingController {
         return wiseSaying;
     }
 
-    public void actionAdd(Scanner scanner, List<WiseSaying> wiseSayings) {
+    public void actionAdd(Scanner scanner) {
         System.out.print("명언 : ");
         String content = scanner.nextLine();
         System.out.print("작가 : ");
@@ -40,7 +43,7 @@ public class WiseSayingController {
         System.out.println("%d번 명령이 등록되었습니다.".formatted(wiseSaying.getId()));
     }
 
-    public void actionList(List<WiseSaying> wiseSayings) {
+    public void actionList() {
         System.out.println(" 번호 / 작가 / 명언 ");
         System.out.println("------------------------");
 
@@ -49,7 +52,7 @@ public class WiseSayingController {
         }
     }
 
-    public void actionDelete(String cmd, List<WiseSaying> wiseSayings) {
+    public void actionDelete(String cmd) {
         String idStr = cmd.substring(6);
         int id = Integer.parseInt(idStr);
         boolean removed = wiseSayings.removeIf(wiseSaying -> wiseSaying.getId() == id);
@@ -58,7 +61,7 @@ public class WiseSayingController {
         else System.out.println("%d번 명언은 존재하지않습니다.".formatted(id));
     }
 
-    public void actionModify(Scanner scanner, String cmd, List<WiseSaying> wiseSayings) {
+    public void actionModify(Scanner scanner, String cmd) {
         String idStr = cmd.substring(6);
         int id = Integer.parseInt(idStr);
         WiseSaying foundWaseSaying = null;
